3459a318ad4be858967ba1cc7886b11c2364f322
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-08-15 10:23:42 -0500

    rename edit:moveList -> turnList

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -17,21 +17,21 @@
 
       v-divider(vertical)
       v-col(cols=3)
-        v-window(v-model="moveListIndex")
-          v-window-item(v-for="(moveList, index) in moveLists", :key="index")
+        v-window(v-model="turnListIndex")
+          v-window-item(v-for="(turnList, index) in turnLists", :key="index")
             v-simple-table
               tbody
-                tr(v-for="(moves, turnNumber) in moveList")
+                tr(v-for="(turn, turnNumber) in turnList")
                   td {{ turnNumber + 1 }}
-                  td {{ moves.whiteMove.san }}
-                  td(v-if="moves.blackMove !== undefined") {{ moves.blackMove.san }}
+                  td {{ turn.whiteMove.san }}
+                  td(v-if="turn.blackMove !== undefined") {{ turn.blackMove.san }}
         
-        div(v-if="moveLists.length > 1")
-          v-btn(large @click="moveListIndex--" :disabled="doNotAllowMoveListPrevious")
+        div(v-if="turnLists.length > 1")
+          v-btn(large @click="turnListIndex--" :disabled="doNotAllowturnListPrevious")
             v-icon mdi-chevron-left
             div Previous
 
-          v-btn(large @click="moveListIndex++" :disabled="doNotAllowMoveListNext")
+          v-btn(large @click="turnListIndex++" :disabled="doNotAllowturnListNext")
             div Next
             v-icon mdi-chevron-right
 
@@ -52,11 +52,11 @@ import { Threats } from "@/components/chessboard.vue";
 import { Side } from "@/store/side";
 
 class EditData {
-  moveListIndex: number;
+  turnListIndex: number;
   activePosition: RepertoirePosition;
 
   constructor() {
-    this.moveListIndex = 0;
+    this.turnListIndex = 0;
     this.activePosition = new RepertoirePosition(
       "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
       true,
@@ -76,16 +76,16 @@ export default Vue.extend({
   computed: {
     ...mapState(["repertoire"]),
 
-    moveLists(): Array<Array<Turn>> {
+    turnLists(): Array<Array<Turn>> {
       return this.activePosition.GetTurnLists();
     },
 
-    doNotAllowMoveListPrevious(): boolean {
-      return this.moveListIndex === 0;
+    doNotAllowturnListPrevious(): boolean {
+      return this.turnListIndex === 0;
     },
 
-    doNotAllowMoveListNext(): boolean {
-      return this.moveListIndex === this.moveLists.length - 1;
+    doNotAllowturnListNext(): boolean {
+      return this.turnListIndex === this.turnLists.length - 1;
     }
   },
 
@@ -93,7 +93,7 @@ export default Vue.extend({
     ...mapMutations(["addRepertoirePosition"]),
 
     updateBoard(position: RepertoirePosition): void {
-      this.moveListIndex = 0;
+      this.turnListIndex = 0;
 
       this.activePosition = position;
     },
