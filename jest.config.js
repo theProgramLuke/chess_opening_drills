bf2d30357e2d0d6ea9450acadd27ba17e718706e
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-08-28 11:36:48 -0500

    Include vue components in coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.{js,ts,vue}",
+    // vue CLI files
+    "!src/shims-*.d.ts",
+    "!src/registerServiceWorker.ts",
+    "!src/main.ts",
+    "!src/router/index.ts",
+    "!src/background.ts",
+    "!src/plugins/**",
+    // Generated pegjs file
+    "!src/store/pgnGrammar.ts",
+    // Generated Plotly themes
+    "!src/views/PlotlyLayouts.ts"
+  ],
+  moduleFileExtensions: ["ts", "vue", "js"],
+  transform: {
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+  },
+  globals: {
+    "vue-jest": {
+      pug: { doctype: "html" }
+    }
+  }
 };
