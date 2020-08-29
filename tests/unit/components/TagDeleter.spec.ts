4bd03b4854449b823655b2ee4848c85b88ae3286
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-08-28 19:18:56 -0500

    Add tagDeleter onDelete test

--- tests/unit/components/TagDeleter.spec.ts
+++ tests/unit/components/TagDeleter.spec.ts
@@ -27,4 +27,39 @@ describe("TagDeleter", () => {
 
     expect(component.element).toMatchSnapshot();
   });
+
+  it("should default to not showing a delete dialog", () => {
+    const component = shallowMount(TagDeleter, {
+      propsData: {
+        tag: new RepertoireTag(
+          Side.White,
+          "tag name",
+          new RepertoirePosition("", "", Side.White),
+          "",
+          []
+        )
+      }
+    });
+
+    expect(component.vm.$data.showDialog).toBeFalsy();
+  });
+
+  it("should emit onDelete for the supplied tag", async () => {
+    const tag = new RepertoireTag(
+      Side.White,
+      "tag name",
+      new RepertoirePosition("", "", Side.White),
+      "",
+      []
+    );
+    const component = shallowMount(TagDeleter, {
+      propsData: {
+        tag: tag
+      }
+    });
+
+    (component.vm as any & { onDelete: () => void }).onDelete();
+
+    expect(component.emitted().onDelete).toEqual([[tag]]);
+  });
 });
