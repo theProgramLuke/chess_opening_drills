8e5846f2699a2e429d475318565182c403333fb1
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-09-05 17:44:23 -0500

    Disambiguate position tags

--- src/components/reports/Difficulty.vue
+++ src/components/reports/Difficulty.vue
@@ -2,22 +2,25 @@
   v-container.fill-height.ma-0(fluid)
     template(v-if="showNoPositions")
       v-alert(color="error") No positions have been trained
-    template(v-else)
-      v-tooltip(bottom)
-        template(v-slot:activator="{ on }")
-          v-chip(v-on="on", label) What is difficulty
-            v-icon(right) mdi-comment-question-outline
+    v-row(v-else)
+      v-col(cols="auto")
+        template
+          v-tooltip(bottom)
+            template(v-slot:activator="{ on }")
+              v-chip(v-on="on", label) What is difficulty
+                v-icon(right) mdi-comment-question-outline
 
-        div Difficulty is the memory retention easiness factor from the SM-2 algorithm.
-        div An easiness factor of 1 indicates a very hard position to remember,
-        span higher easiness factors are easier to remember.
-        div The initial easiness factor for a position is 2.5.
+            p Difficulty is the memory retention easiness factor from the SM-2 algorithm.
+            | An easiness factor of 1 indicates a very hard position to remember,
+            | higher easiness factors are easier to remember.
+            | The initial easiness factor for a position is 2.5.
 
-      plot(
-        :data="data",
-        :layout="layout",
-        :options="options",
-          :dark="darkMode")
+      v-col.fill-height(cols=9)
+        plot.ma-10(
+          :data="data",
+          :layout="layout",
+          :options="options",
+            :dark="darkMode")
 </template>
 
 <script lang="ts" src="./DifficultyViewModel.ts" />
