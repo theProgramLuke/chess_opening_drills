6324779cdecef462f7e6e7310cb489cee79e2b2a
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-09-09 19:31:30 -0500

    Loading indicatior on enable engine

--- src/components/edit/EngineRecommendations.vue
+++ src/components/edit/EngineRecommendations.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
     v-container
         v-switch(v-model="activeEngine", label="Enable engine", @change="activateEngine")
+        v-progress-circular(
+            v-if="activeEngine && sortedEngineRecommendations.length === 0",
+            indeterminate)
         p(v-for="recommendation in sortedEngineRecommendations")
             | {{ recommendation.evaluation }}
             | {{ recommendation.variation[0] }}
