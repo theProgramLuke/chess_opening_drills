0bbdf4dedb1349551109843639c964b0531e6e45
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-09-13 19:25:45 -0500

    Show Learned Positions

---
+++ src/components/reports/Learned.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  v-container.fill-height.ma-0(fluid)
+    template(v-if="showNoPositions")
+      v-alert(color="error") No positions have been entered
+
+    plot(
+      v-else,
+      :data="plotData",
+      :layout="layout",
+      :options="options",
+      :dark="darkMode")
+</template>
+
+<script lang="ts" src="./LearnedViewModel.ts" />
