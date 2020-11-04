437c47203708dc0e8ca1d2ee91113071c28a9025
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-11-03 21:57:59 -0600

    0.6.1

--- release_scripts/ci.sh
+++ release_scripts/ci.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-yarn --link-duplicates --pure-lockfile
+set
+
+yarn --link-duplicates --pure-lockfile || exit 1
 yarn test:unit || exit 1
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     ORIG_CMD=$TRAVIS_CMD
     unset TRAVIS_CMD
-    docker run --rm \
+    (docker run --rm \
         --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
         -v ${PWD}:/project \
         -v ~/.cache/electron:/root/.cache/electron \
         -v ~/.cache/electron-builder:/root/.cache/electron-builder \
         electronuserland/builder:wine \
-        /bin/bash -c "yarn release --linux --win"
+        /bin/bash -c "yarn release --linux --win") || exit 1
     TRAVIS_CMD=$ORIG_CMD
     unset ORIG_CMD
 else
