4587b6f0e5243d931bc1a2032f66f606d28adcba
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-11-04 09:47:55 -0600

    auto updates

--- src/background.ts
+++ src/background.ts
@@ -4,6 +4,8 @@ import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 import path from "path";
+import { autoUpdater } from "electron-updater";
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 declare const __static: string; // https://github.com/electron-userland/electron-webpack/issues/172
 
@@ -36,6 +38,8 @@ function createWindow() {
     createProtocol("app");
     // Load the index.html when not in development
     win.loadURL("app://./index.html");
+
+    autoUpdater.checkForUpdatesAndNotify();
   }
 
   win.on("closed", () => {
