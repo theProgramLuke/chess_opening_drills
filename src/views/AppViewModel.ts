9778aebc081149ede7cdc7748d24aed2dc8b0b7c
theProgramLuke <4632542+theProgramLuke@users.noreply.github.com>
2020-11-24 21:27:44 -0600

    Scrollable settings (#117)
    * scrollable settings
    
    * Remove unused declaration

--- src/views/AppViewModel.ts
+++ src/views/AppViewModel.ts
@@ -3,6 +3,7 @@ import { Vue, Component, Watch } from "vue-property-decorator";
 import { State } from "vuex-class";
 import _ from "lodash";
 import { Repertoire } from "@/store/repertoire/Repertoire";
+import { Location } from "vue-router";
 
 export interface NavigationMenuItem {
   name: string;
@@ -115,6 +116,19 @@ export default class AppViewModel extends Vue {
     const multipleMoves = repertoire.training.getPositionsWithMultipleTrainings();
 
     return _.size(multipleMoves);
+  }
+
+  @Watch("$route")
+  onRouteChange(route: Location): void {
+    const element = _.first(document.children);
+
+    if (element) {
+      if (route.name === "Settings" || route.name === "Welcome") {
+        element.classList.add("scrollable");
+      } else {
+        element.classList.remove("scrollable");
+      }
+    }
   }
 
   @Watch("darkMode")
